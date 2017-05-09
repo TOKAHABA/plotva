3b40fb0b279a8ffc36c53cba426552d832a03961
Toka <tokacore@example.com>
2017-05-10 00:05:34 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Plotva | home</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <!-- wrapper for footer to bottom start -->
+    <div id="top" class="page-wrapper">
+
+      <!-- nav start -->
+      <nav class="navbar navbar-default">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Plotva.inc</a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Каталог товаров <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Раздел 1</a></li>
+                  <li><a href="#">Раздел 2</a></li>
+                  <li><a href="#">Раздел 3</a></li>
+                </ul>
+              </li>
+              <li><a href="news.php">Новости</a></li>
+              <li><a href="delivery.php">Доставка</a></li>
+              <li><a href="about.php">О нас</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="" data-toggle="modal" data-target="#autorization">Личный кабинет</a></li>
+              <li><a href="#">LoginPolzovatelya</a></li>
+              <li><a href="#" class="m-r--15">Корзина</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+      <!-- nav end -->
+
+      <!-- main content start -->
+      <div class="container">
+        <!-- carousel-start -->
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner carousel__sliders-box" role="listbox">
+            <div class="item active">
+              <img src="img/example.jpg" alt="...">
+              <div class="carousel-caption">
+                Slide #1
+              </div>
+            </div>
+            <div class="item">
+              <img src="img/example.jpg" alt="...">
+              <div class="carousel-caption">
+                Slide #2
+              </div>
+            </div>
+            <div class="item">
+              <img src="img/example.jpg" alt="...">
+              <div class="carousel-caption">
+                Slide #3
+              </div>
+            </div>
+          </div>
+
+          <!-- Controls -->
+          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        <!-- carousel end -->
+        </div>
+
+        <div class="row">
+
+          <ul class="col-sm-4 hidden-xs sections-list">
+            <li class="sections-list__item">
+              <h2 class="sections-list__header"><a href="#" class="sections-list__link">Раздел №1</a></h2>
+              <img src="img/example.jpg" alt="sections img" class="sections-list__img">
+            </li>
+            <li class="sections-list__item">
+              <h2 class="sections-list__header"><a href="#" class="sections-list__link">Раздел №2</a></h2>
+              <img src="img/example.jpg" alt="sections img" class="sections-list__img">
+            </li>
+            <li class="sections-list__item">
+              <h2 class="sections-list__header"><a href="#" class="sections-list__link">Раздел №3</a></h2>
+              <img src="img/example.jpg" alt="sections img" class="sections-list__img">
+            </li>
+          </ul>
+
+          <div class="col-xs-12 col-sm-8">
+            <ul class="news-list__box">
+              <li class="news-list__item">
+                <h2 class="news-list__head">Заголовок новости</h2>
+                <p class="news-list__text text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit repellat deleniti dicta delectus, ipsam, modi quia dolor odit error rem adipisci quasi quos eum perspiciatis magnam aspernatur. Repellat sint explicabo ipsa laboriosam. Eos ut doloremque veniam. Obcaecati iusto eligendi, voluptatem, commodi necessitatibus, ipsam veritatis aspernatur reiciendis, nesciunt accusantium incidunt possimus sapiente sit et repellat maxime adipisci nam aliquid. A autem distinctio numquam mollitia nostrum, reprehenderit quaerat. Aut id, fugiat totam fugit distinctio deserunt cum nam maxime animi nulla natus dolor, laboriosam excepturi, repellat, cupiditate dolores perspiciatis. Voluptatem, quisquam iste quod! Deleniti, tempora perspiciatis ea! Impedit minus atque illum, veritatis. Dignissimos harum dolorem quis dolor repudiandae magnam a deleniti nihil dicta sapiente, possimus! Reiciendis eos autem dolores quo iusto pariatur, animi.</p>
+                <a href="#">Подробнее...</a>
+              </li>
+              <li class="news-list__item">
+                <h2 class="news-list__head">Заголовок новости</h2>
+                <p class="news-list__text text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, qui enim nulla commodi reiciendis alias!</p>
+                <a href="#">Подробнее...</a>
+              </li>
+              <li class="news-list__item">
+                <h2 class="news-list__head">Заголовок новости</h2>
+                <p class="news-list__text text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem optio quos autem quasi nihil tempore architecto tenetur, natus velit libero ipsa dignissimos, id sit rerum fugiat voluptates perferendis, maiores sed? Iusto vitae qui accusantium sequi debitis, officiis voluptas perspiciatis. Vel nemo recusandae optio ab, sint saepe iusto, eaque architecto, necessitatibus est quasi quidem delectus magni, quaerat ex rem libero veritatis!</p>
+                <a href="#">Подробнее...</a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      <!-- container end -->
+      </div>
+      <!-- main content end -->
+
+      <!-- buffer for footer to bottom -->
+      <div class="page-buffer"></div>
+    <!-- wrapper for footer to bottom end -->
+    </div>
+    <!-- footer -->
+    <footer class="page-footer footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <a href="#top" class="footer-header">Plotva.inc</a>
+          </div>
+          <div class="col-sm-4">
+            <ul class="footer-sections__list">
+              <li><a href="#" class="footer-sections-list__link">Раздел 1</a></li>
+              <li><a href="#" class="footer-sections-list__link">Раздел 2</a></li>
+              <li><a href="#" class="footer-sections-list__link">Раздел 3</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-4">
+            <ul class="footer-social__list">
+              <li><a href="#"><i class="fa fa-vk" aria-hidden="true"></i><span class="footer-social__text">ВКонтакте</a></li>
+              <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i><span class="footer-social__text">Facebook</span></a></li>
+              <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i><span class="footer-social__text">Instagram</span></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- modal start -->
+    <div class="modal fade" id="autorization" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Войти в личный кабинет</h4>
+          </div>
+          <div class="modal-body">
+
+          <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingTwo">
+                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Регистрация
+                </a>
+              </div>
+              <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="panel-body">
+                  <form>
+                    <div class="form-group">
+                      <label for="RegistrationEml">E-mail</label>
+                      <input type="email" class="form-control" id="RegistrationEml" placeholder="Ваш E-mail">
+                    </div>
+                    <div class="form-group">
+                      <label for="RegistrationLogin">Логин</label>
+                      <input type="" class="form-control" id="RegistrationLogin" placeholder="Ваш логин">
+                    </div>
+                    <div class="form-group">
+                      <label for="RegistrationPwd">Пароль</label>
+                      <input type="password" class="form-control" id="RegistrationPwd" placeholder="Ваш пароль">
+                    </div>
+                    <button type="submit" class="btn btn-default">Зарегистрироваться</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingOne">
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Авторизация
+                </a>
+              </div>
+              <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                  <form>
+                    <div class="form-group">
+                      <label for="AutorizationLogin">Логин или E-mail</label>
+                      <input type="" class="form-control" id="AutorizationLogin" placeholder="Ваш логин">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Пароль</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ваш пароль">
+                    </div>
+                    <div class="checkbox">
+                      <label>
+                        <input type="checkbox"> Запомнить меня
+                      </label>
+                    </div>
+                    <button type="submit" class="btn btn-default">Войти</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- modal end -->
+    
+    <!-- JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
